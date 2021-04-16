900ba3b51fb5c3ba36b433bc7e0d10d070149899
Erick Contreras <Erickcc06@gmail.com>
2021-04-15 23:18:38 -0600

    Formatted and commented code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Add IDs for buttons
 var timeBtn9 = $('#time9');
 var timeBtn10 = $('#time10');
 var timeBtn11 = $('#time11');
@@ -18,103 +19,105 @@ var text16 = $('#text16');
 var text17 = $('#text17');
 var container = $('.container');
 
-
+//Get current date and display it with the following format in the header : dayName, month dayNumber
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-
-timeBtn9.on('click', function(){
+//Add event listener for the save buttons all the different hours
+timeBtn9.on('click', function () {
     localStorage.setItem("storedText9", text9.val());
 });
-timeBtn10.on('click', function(){
+timeBtn10.on('click', function () {
     localStorage.setItem("storedText10", text10.val());
 });
-timeBtn11.on('click', function(){
+timeBtn11.on('click', function () {
     localStorage.setItem("storedText11", text11.val());
 });
-timeBtn12.on('click', function(){
+timeBtn12.on('click', function () {
     localStorage.setItem("storedText12", text12.val());
 });
-timeBtn13.on('click', function(){
+timeBtn13.on('click', function () {
     localStorage.setItem("storedText13", text13.val());
 });
-timeBtn14.on('click', function(){
+timeBtn14.on('click', function () {
     localStorage.setItem("storedText14", text14.val());
 });
-timeBtn15.on('click', function(){
+timeBtn15.on('click', function () {
     localStorage.setItem("storedText15", text15.val());
 });
-timeBtn16.on('click', function(){
+timeBtn16.on('click', function () {
     localStorage.setItem("storedText16", text16.val());
 });
-timeBtn17.on('click', function(){
+timeBtn17.on('click', function () {
     localStorage.setItem("storedText17", text17.val());
 });
 
-
-function updateScreenText(){
+//Retrieves the local stored information and updates the text area with said information for each hour
+function updateScreenText() {
     var currentText;
-    if(localStorage.getItem("storedText9") !== null){
+    if (localStorage.getItem("storedText9") !== null) {
         currentText = localStorage.getItem("storedText9");
         text9.val(currentText);
     }
-    if(localStorage.getItem("storedText10") !== null){
+    if (localStorage.getItem("storedText10") !== null) {
         currentText = localStorage.getItem("storedText10");
         text10.val(currentText);
     }
-    if(localStorage.getItem("storedText11") !== null){
+    if (localStorage.getItem("storedText11") !== null) {
         currentText = localStorage.getItem("storedText11");
         text11.val(currentText);
     }
-    if(localStorage.getItem("storedText12") !== null){
+    if (localStorage.getItem("storedText12") !== null) {
         currentText = localStorage.getItem("storedText12");
         text12.val(currentText);
     }
-    if(localStorage.getItem("storedText13") !== null){
+    if (localStorage.getItem("storedText13") !== null) {
         currentText = localStorage.getItem("storedText13");
         text13.val(currentText);
     }
-    if(localStorage.getItem("storedText14") !== null){
+    if (localStorage.getItem("storedText14") !== null) {
         currentText = localStorage.getItem("storedText14");
         text14.val(currentText);
     }
-    if(localStorage.getItem("storedText15") !== null){
+    if (localStorage.getItem("storedText15") !== null) {
         currentText = localStorage.getItem("storedText15");
         text15.val(currentText);
     }
-    if(localStorage.getItem("storedText16") !== null){
+    if (localStorage.getItem("storedText16") !== null) {
         currentText = localStorage.getItem("storedText16");
         text16.val(currentText);
     }
-    if(localStorage.getItem("storedText17") !== null){
+    if (localStorage.getItem("storedText17") !== null) {
         currentText = localStorage.getItem("storedText17");
         text17.val(currentText);
     }
 }
 
-
+//Adds color to each text area
 function updateScreenColors() {
+    //We handle the hours of the day in a 24hrs format
     var currentHour = today.format('H');
-    
+    //Daily work hours go from 9hrs to 17hrs
     currentText = 9;
     var textIDs = [text9, text10, text11, text12, text13, text14, text15, text16, text17];
-    
-    // asd[0].addClass("future");
+    for (var i = 9; i < 18; i++) {
+        //If the stored text area belongs to a future hour, then the text area turns green
+        if (currentHour < currentText) {
+            textIDs[i - 9].addClass("future");
+            //If the stored text area belongs to the present hour, then turn the area red
+        } else if (currentHour == currentText) {
+            textIDs[i - 9].addClass("present");
+        } else {
+            //If the stored text area  belongs to an hour that already passed, then turn the area grayish
+            textIDs[i - 9].addClass("past");
 
-    //Evaluate for 9,10,11,12 hours
-    for (var i = 9; i < 18; i++){
-        if (currentHour < currentText){
-            textIDs[i-9].addClass("future");
-        }else if(currentHour == currentText){
-            textIDs[i-9].addClass("present");
-        }else{
-            textIDs[i-9].addClass("past");
-            console.log("current hour: " + currentHour);
-            console.log("current hour: " + currentText);
         }
+        //Variable that we use to move to different text boxes
         currentText++;
     }
 }
 
+//On load, the screen updates the text areas for each area with the stored information
 updateScreenText();
+// On load, the screen updates the color for each text area depending on what time it is
 updateScreenColors();
